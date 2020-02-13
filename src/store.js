def38187a99eb2e1661f263f53fa8b4b4f9fc402
Casey Martell <drmartell@gmail.com>
2020-02-13 13:29:59 -0800

    connects index.js to the redux store

---
+++ src/store.js
@@ -0,0 +1,4 @@
+const { createStore } = require.apply('redux');
+import reducer from './reducers/reducer';
+
+export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
